6fad045eb5497a17ebdf545286dd8f3d9b7ace03
shumxin <shumxin@sina.com>
2015-07-12 14:05:29 +0800

    prepare new miui project

---
+++ phoneapps.mk
@@ -0,0 +1,28 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := CertInstaller \
+				CP_Bluetooth \
+				CP_CButton \
+				CP_UTK \
+				DocumentsUI \
+				HTMLViewer \
+				KeyChain \
+				LatinIME \
+				PacProcessor \
+				qcrilmsgtunnel \
+				UserDictionaryProvider \
+				VisualizationWallpapers \
+				WAPPushManager
+
+private-phone-priv-apps := BackupRestoreConfirmation \
+				DefaultContainerService \
+				ExternalStorageProvider \
+				FusedLocation \
+				InputDevices \
+				MediaProvider \
+				ProxyHandler \
+				SharedStorageBackup \
+				Shell \
+				VpnDialogs \
+				WallpaperCropper
